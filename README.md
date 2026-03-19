ac92bdbb40524a000c736f6be0319ca2a983ef00
Poissott <ottpoiss8@gmail.com>
2026-03-19 23:11:06 +0200

    initial structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# viktoriinirakendus
+# Viktoriinirakendus
+
+
---
+++ src/data/questions.ts
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    question: "Mis on Eesti pealinn?",
+    options: [
+      "Tallinn",
+      "Tartu",
+      "Narva"
+    ],
+    answer: "Tallinn"
+  },
+  {
+    question: "Mis on Eesti suuruselt teine linn?",
+    options: [
+      "Pärnu",
+      "Tartu",
+      "Narva",
+      "Viljandi"
+    ],
+    answer: "Tartu"
+  },
+  {
+    question: "Mis on Eesti pikim jõgi?",
+    options: [
+      "Emajõgi",
+      "Pärnu jõgi",
+      "Võhandu jõgi",
+      "Pirita jõgi"
+    ],
+    answer: "Võhandu jõgi"
+  },
+  {
+    question: "Mis on Eesti suurim saar?",
+    options: [
+      "Muhu",
+      "Vormsi",
+      "Saaremaa",
+      "Hiiumaa",
+      "Kihnu"
+    ],
+    answer: "Saaremaa"
+  },
+]
+
+export default questions;
