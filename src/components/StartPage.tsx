3ad4ea4620ba2931047e0e8886c9d050c611c2c1
Poissott <ottpoiss8@gmail.com>
2026-03-21 19:54:23 +0200

    startpage design

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -2,9 +2,13 @@ import { Link } from "react-router-dom";
 
 function StartPage() {
   return (
-    <div className="start-page">
-      <h1>Tere tulemast viktoriinirakendusse!</h1>
-      <Link to="/kysimus/1">Alusta viktoriini</Link>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-white flex flex-col items-center justify-center p-25">
+        <h1 className="text-3xl font-bold mb-4">Tere tulemast viktoriinirakendusse!</h1>
+        <Link to="/kysimus/1" className="bg-black text-white hover:bg-white hover:text-black border border-black border-2 font-bold py-2 px-4">
+          Alusta viktoriini
+        </Link>
+      </div>
     </div>
   );
 }
